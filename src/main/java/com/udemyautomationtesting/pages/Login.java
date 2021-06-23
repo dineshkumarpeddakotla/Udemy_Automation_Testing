3d2a9db458a7a9a124bbcea2e19a6eb902c142b3
dinnu247 <dineshkumar.icon@gmail.com>
2021-06-23 19:53:00 +0530

    ADDED:Different types of login methods

---
+++ src/main/java/com/udemyautomationtesting/pages/Login.java
@@ -0,0 +1,155 @@
+package com.udemyautomationtesting.pages;
+
+import com.udemyautomationtesting.base.Base;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.Set;
+
+public class Login extends Base {
+    //object reference is declared
+    WebDriver driver;
+
+    //object references are declared and uses FindBy annotation to locate the web element
+    @FindBy(xpath = "//span[contains(text(),'Log in')]")
+    WebElement loginLink;
+    @FindBy(name = "email")
+    WebElement email;
+    @FindBy(name = "password")
+    WebElement password;
+    @FindBy(xpath = "//INPUT[@id='submit-id-submit']")
+    WebElement login;
+    @FindBy(linkText = "Continue with Google")
+    WebElement continueWithGoogle;
+    @FindBy(id = "identifierId")
+    WebElement googleEmail;
+    @FindBy(linkText = "Next")
+    WebElement next;
+    @FindBy(name = "password")
+    WebElement googlePassword;
+    @FindBy(linkText = "Continue with Facebook")
+    WebElement continueWithFacebook;
+    @FindBy(name = "pass")
+    WebElement facebookPassword;
+    @FindBy(id = "loginbutton")
+    WebElement fLogin;
+
+    //parameterized constructor is used
+    public Login(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this); //initElements method is used from PageFactory class
+    }
+
+    //multiple methods are created for multiple actions
+    private void clickLoginLink() {
+        loginLink.click();
+    }
+
+    public void setEmail(String inputEmail) {
+        email.sendKeys(inputEmail);
+    }
+
+    public void setPassword(String inputPassword) {
+        password.sendKeys(inputPassword);
+    }
+
+    public void clickLogin() {
+        login.click();
+    }
+
+    public void clickContinueWithGoogle() {
+        continueWithGoogle.click();
+    }
+
+    public void setGoogleEmail(String inputGoogleEmail) {
+        googleEmail.sendKeys(inputGoogleEmail);
+    }
+
+    public void clickNext() {
+        next.click();
+    }
+
+    public void setGooglePassword(String inputGooglePassword) {
+        googlePassword.sendKeys(inputGooglePassword);
+    }
+
+    public void clickContinueWithFacebook() {
+        continueWithFacebook.click();
+    }
+
+    public void setFacebookPassword(String inFacebookPassword) {
+        facebookPassword.sendKeys(inFacebookPassword);
+    }
+
+    public void clickFLogin() {
+        fLogin.click();
+    }
+
+    /**
+     * login method is used to login into application
+     * @return page title
+     * @throws InterruptedException interrupts the execution for certain period
+     */
+    public String login() throws InterruptedException {
+        clickLoginLink();
+        setEmail("dineshkumar.icon@gmail.com");
+        setPassword("Dinnu@247");
+        clickLogin();
+
+        Thread.sleep(2000);
+        return driver.getTitle();
+    }
+
+    /**
+     * loginWithGoogle method is used to login into application with google account
+     * @return page title
+     * @throws InterruptedException interrupts the execution for certain period
+     */
+    public String loginWithGoogle() throws InterruptedException {
+        clickLoginLink();
+        clickContinueWithGoogle();
+        Thread.sleep(1000);
+
+        Set<String> windowHandles = driver.getWindowHandles();
+        System.out.println(windowHandles);
+
+        for (String winHandle : windowHandles) {
+            driver.switchTo().window(winHandle);
+            System.out.println(winHandle);
+            String actual = driver.getTitle();
+            System.out.println(actual);
+            System.out.println("Before if");
+
+            if (actual.equals("Sign in – Google accounts")) {
+                System.out.println("After if");
+                setGoogleEmail("dineshkumar.icon@gmail.com");
+                clickNext();
+                Thread.sleep(3000);
+                setGooglePassword("Dinnu@366247");
+                clickNext();
+                Thread.sleep(3000);
+            }
+        }
+
+        return driver.getTitle();
+    }
+
+    /**
+     * loginWithFacebook method is used to login into application with facebook
+     * @return page title
+     * @throws InterruptedException interrupts the execution for certain period
+     */
+    public String loginWithFacebook() throws InterruptedException {
+        clickLoginLink();
+        clickContinueWithFacebook();
+        Thread.sleep(3000);
+        setEmail("dineshkumar.icon@gmail.com");
+        setFacebookPassword("dinnu247");
+        clickFLogin();
+        Thread.sleep(1000);
+
+        return driver.getTitle();
+    }
+}
